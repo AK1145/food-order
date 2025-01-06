e738661e59d1baad7b89d13d815019dc3449efec
Arulkumar M <154044042+AK1145@users.noreply.github.com>
2025-01-06 11:09:49 +0530

    altered foodorder.java

--- foodorder.java
+++ foodorder.java
@@ -3,63 +3,63 @@ class foodorder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Tomato your online kitchen");
-        System.out.println("Choose between the categories\n1.Veg \n2.Non-veg");
+        System.out.println("Choose between the categories\n\t1.Veg \n\t2.Non-veg");
         int a = sc.nextInt();
         int q;
         float p;
         if(a == 1){
             while(true) {
-                System.out.println("You have choosed veg.\nPlease select the variety you want to continue the order with");
-                System.out.println("1.Vegetable biryani \n2.Mushroom biryani \n3.Fried rice \n4.Noodles \n5.Pizza");
+                System.out.println("\nYou have choosed veg.\nPlease select the variety you want to continue the order with");
+                System.out.println("\t1.Vegetable biryani \n\t2.Mushroom biryani \n\t3.Fried rice \n\t4.Noodles \n\t5.Pizza");
                 int choice = sc.nextInt();
                 switch(choice){
                     case 1:
-                    System.out.println("You have choosen Vegetable biryani.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Vegetable biryani.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 100;
                     System.out.println("Price of 1 quantity = 100\nYour total price = "+p);
                     break; 
     
                     case 2:
-                    System.out.println("You have choosen Mushroom biryani.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Mushroom biryani.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 100;
                     System.out.println("Price of 1 quantity = 100\nYour total price = "+p);
                     break;
     
                     case 3:
-                    System.out.println("You have choosen Fried rice.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Fried rice.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 100;
                     System.out.println("Price of 1 quantity = 100\nYour total price = "+p);
                     break;
     
                     case 4:
-                    System.out.println("You have choosen Noodles.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Noodles.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 100;
                     System.out.println("Price of 1 quantity = 100\nYour total price = "+p);
                     break;
     
                     case 5:
-                    System.out.println("You have choosen Pizza.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Pizza.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 100;
                     System.out.println("Price of 1 quantity = 100\nYour total price = "+p);
                     break;
                 }
-                System.out.println("Do you want to order any other items\n1.yes \n2.no");
+                System.out.println("\nDo you want to order any other items\n\t1.yes \n\t2.no");
                 int any = sc.nextInt();
                 if(any == 1){
                     continue;
                 }
                 else{
-                    System.out.println("Thanks for ordering.\nSelect the hotel you want to continue the order with");
+                    System.out.println("\nThanks for ordering.\nSelect the hotel you want to continue the order with");
                 }
                 break;
             }
@@ -67,75 +67,75 @@ class foodorder {
         }
         else {
            while(true) {
-                System.out.println("You have choosed Non-veg.\nPlease select the variety you want to continue the order with");
-                System.out.println("1.Chicken biryani \n2.Mutton biryani \n3.fried rice \n4.Tandoori \n5.Grill");
+                System.out.println("\nYou have choosed Non-veg.\nPlease select the variety you want to continue the order with");
+                System.out.println("\t1.Chicken biryani \n\t2.Mutton biryani \n\t3.fried rice \n\t4.Tandoori \n\t5.Grill");
                 int choice = sc.nextInt();
                 switch(choice){
                     case 1:
-                    System.out.println("You have choosen Chicken biryani.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Chicken biryani.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 150;
                     System.out.println("Price of 1 quantity = 150\nYour total price = "+p);
                     break; 
     
                     case 2:
-                    System.out.println("You have choosen Mutton biryani.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Mutton biryani.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 150;
                     System.out.println("Price of 1 quantity = 150\nYour total price = "+p);
                     break;
     
                     case 3:
-                    System.out.println("You have choosen Fried rice.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Fried rice.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 150;
                     System.out.println("Price of 1 quantity = 150\nYour total price = "+p);
                     break;
     
                     case 4:
-                    System.out.println("You have choosen Tandoori.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Tandoori.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 150;
                     System.out.println("Price of 1 quantity = 150\nYour total price = "+p);
                     break;
     
                     case 5:
-                    System.out.println("You have choosen Grill.\nPlease enter the quantity");
+                    System.out.println("\nYou have choosen Grill.\nPlease enter the quantity");
                     q = sc.nextInt();
-                    System.out.println("Your selected quantity : " +q);
+                    System.out.println("\nYour selected quantity : " +q);
                     p = q * 150;
                     System.out.println("Price of 1 quantity = 150\nYour total price = "+p);
                     break;
                 }
-                System.out.println("Do you want to order any other items\n1.yes \n2.no");
+                System.out.println("\nDo you want to order any other items\n\t1.yes \n\t2.no");
                 int any = sc.nextInt();
                 if(any == 1){
                     continue;
                 }
                 else{
-                    System.out.println("Thanks for ordering.\nSelect the hotel you want to continue the order with");
+                    System.out.println("\nThanks for ordering.\nSelect the hotel you want to continue the order with");
                 }
                 break;
             }    
             
         }
-        System.out.println("1.Annapoorna \n2.Gowri Krishna \n3.Hotel Surya \n4.Food Khalifa");
+        System.out.println("\t1.Annapoorna \n\t2.Gowri Krishna \n\t3.Hotel Surya \n\t4.Food Khalifa");
         int hotel = sc.nextInt();
         if(hotel == 1){
-            System.out.println("You have choosen Hotel Annapoorna.\nYour order will be delivered shortly.");
+            System.out.println("\nYou have choosen Hotel Annapoorna.\nYour order will be delivered shortly.");
         }
         else if(hotel == 2){
-            System.out.println("You have choosen Hotel Gowri Krishna.\nYour order will be delivered shortly.");
+            System.out.println("\nYou have choosen Hotel Gowri Krishna.\nYour order will be delivered shortly.");
         }
         else if(hotel == 3){
-            System.out.println("You have choosen Hotel Surya.\nYour order will be delivered shortly.");
+            System.out.println("\nYou have choosen Hotel Surya.\nYour order will be delivered shortly.");
         }
         else{
-            System.out.println("You have choosen Food Khalifa.\nYour order will be delivered shortly.");
+            System.out.println("\nYou have choosen Food Khalifa.\nYour order will be delivered shortly.");
         }
 
 
